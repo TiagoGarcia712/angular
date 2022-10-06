e844681d1efb7b1457be971a6a5f9600775da79d
Tiago_Costa17 <tiago.costa@duotecnologia.com.br>
2022-10-06 12:01:02 -0300

    primeiro commit

---
+++ curso-angular/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  userName = 'Joaquim';
+
+  userData = {
+    email: 'joaquim@email.com',
+    role: 'admin',
+  };
+  title = 'curso-angular';
+}
